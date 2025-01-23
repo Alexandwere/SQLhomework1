b442819f58b7d9407f1ee9852bb1eaed18efb263
Alexandwere <[lazukov.ao@gmail.com]>
2025-01-23 17:14:50 +0300

    refactor code

--- part_2_filling.sql
+++ part_2_filling.sql
@@ -1,12 +1,14 @@
 --Часть 2. Заполнение данными
-
-insert into facultet values (1, 'Инженерный', 30_000);
-insert into facultet values (2, 'Экономический', 49_000);
-insert into course values ('Инж-1', 1, 1);
-insert into course values ('Эк-1', 1, 2);
-insert into course values ('Эк-4', 4, 2);
-insert into student values (1, 'Петр', 'Петров', 'Петрович', true, 'Инж-1');
-insert into student values (2, 'Иван', 'Иванов', 'Иваныч', false, 'Инж-1');
-insert into student values (3, 'Сергей', 'Михно', 'Иваныч', true, 'Эк-4');
-insert into student values (4, 'Ирина', 'Стоцкая', 'Юрьевна', false, 'Эк-4');
-insert into student values (5, 'Настасья', 'Младич', null, false, 'Эк-1');
+insert into facultet values 
+	(1, 'Инженерный', 30_000),
+	(2, 'Экономический', 49_000);
+insert into course values 
+	('Инж-1', 1, 1), 
+	('Эк-1', 1, 2), 
+	('Эк-4', 4, 2);
+insert into student values 
+	(1, 'Петр', 'Петров', 'Петрович', true, 'Инж-1'),
+	(2, 'Иван', 'Иванов', 'Иваныч', false, 'Инж-1'),
+	(3, 'Сергей', 'Михно', 'Иваныч', true, 'Эк-4'),
+	(4, 'Ирина', 'Стоцкая', 'Юрьевна', false, 'Эк-4'),
+	(5, 'Настасья', 'Младич', null, false, 'Эк-1');
